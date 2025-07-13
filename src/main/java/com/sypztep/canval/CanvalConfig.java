d509aa2df70c984c2cfe7596c36ec2d702aca5b4
chain <chainot@hotmail.co.th>
2025-07-13 20:49:07 +0700

    fix the workflow config file to drop back to default of null

--- src/main/java/com/sypztep/canval/CanvalConfig.java
+++ src/main/java/com/sypztep/canval/CanvalConfig.java
@@ -5,42 +5,26 @@ import com.sypztep.canval.util.identifier.RegistryEntry;
 import com.sypztep.canval.util.resource.FontResource;
 
 public final class CanvalConfig {
-    private static RegistryEntry<FontResource> defaultFont = Fonts.DEFAULT_FONT;
+    private static RegistryEntry<FontResource> defaultFont = null; // Set after Fonts.init()
 
     private static int defaultWindowWidth = 1080;
     private static int defaultWindowHeight = 720;
     private static String defaultWindowTitle = "Canval Engine";
 
-    public static RegistryEntry<FontResource> getDefaultFont() {
-        return defaultFont;
+    public static FontResource getDefaultFont() {
+        return defaultFont != null ? defaultFont.value() : Fonts.DEFAULT_FONT.value();
     }
 
     public static void setDefaultFont(RegistryEntry<FontResource> font) {
         defaultFont = font;
-        System.out.println("Default font changed to: " + font.id());
     }
 
-    public static int getDefaultWindowWidth() {
-        return defaultWindowWidth;
-    }
+    public static int getDefaultWindowWidth() { return defaultWindowWidth; }
+    public static void setDefaultWindowWidth(int width) { defaultWindowWidth = width; }
 
-    public static void setDefaultWindowWidth(int width) {
-        defaultWindowWidth = width;
-    }
+    public static int getDefaultWindowHeight() { return defaultWindowHeight; }
+    public static void setDefaultWindowHeight(int height) { defaultWindowHeight = height; }
 
-    public static int getDefaultWindowHeight() {
-        return defaultWindowHeight;
-    }
-
-    public static void setDefaultWindowHeight(int height) {
-        defaultWindowHeight = height;
-    }
-
-    public static String getDefaultWindowTitle() {
-        return defaultWindowTitle;
-    }
-
-    public static void setDefaultWindowTitle(String title) {
-        defaultWindowTitle = title;
-    }
+    public static String getDefaultWindowTitle() { return defaultWindowTitle; }
+    public static void setDefaultWindowTitle(String title) { defaultWindowTitle = title; }
 }
