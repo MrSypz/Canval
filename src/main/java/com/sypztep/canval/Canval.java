cc5f2d34af35f4b019c65ae486e926b4b282c2b9
chain <chainot@hotmail.co.th>
2025-07-13 20:48:28 +0700

    Rename to Canval

--- src/main/java/com/sypztep/canval/Canval.java
+++ src/main/java/com/sypztep/canval/Canval.java
@@ -4,7 +4,7 @@ import com.sypztep.canval.init.Fonts;
 import com.sypztep.canval.util.Initializer;
 
 
-public final class Carval implements Initializer {
+public final class Canval implements Initializer {
     @Override
     public void initialize() {
         Fonts.init();
